532ad1ab8cb08ca73b8c43ec9c217fee240e7204
Nickolay Semendyaev <agent00791@gmail.com>
2021-08-16 14:02:26 +0200

    freezerlte: Enable BOARD_USES_CYANOGEN_HARDWARE
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I51e5cb071bce9a49fd92b7eb59e2f360c27d7d0a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,6 +48,9 @@ BOARD_CHARGER_SHOW_PERCENTAGE := true
 BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/BOOT/BOOT/boot/boot_mode
 
+# CMHW
+BOARD_USES_CYANOGEN_HARDWARE := true
+
 # Dexpreopt
 WITH_DEXPREOPT_BOOT_IMG_ONLY := true
 DONT_DEXPREOPT_PREBUILTS := true
