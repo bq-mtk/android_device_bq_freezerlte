c1230464dc593ac1bafb5a089aaaae0189c509df
padarshr <padarshr@codeaurora.org>
2021-08-16 12:48:19 +0200

    freezerlte: Enable BOARD_HAVE_BLUETOOTH
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: Ic05ecd04fa5d3c7f822dd582c3fb428876a05eef

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,6 +37,9 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+
 # Kernel
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_SOURCE := kernel/bq/mt6753
